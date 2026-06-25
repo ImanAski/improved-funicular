efae1c69fe093bc9729b83fd41e598df26db3f31
Iman Mokhtariaski <imanskater@outlook.com>
2026-06-26 01:45:33 +0330

    .

---
+++ include/lovyanGfxSetup.h
@@ -0,0 +1,100 @@
+#define LGFX_USE_V1
+#include <LovyanGFX.hpp>
+
+#define SCREEN_WIDTH 240
+#define SCREEN_HEIGHT 320
+
+class LGFX : public lgfx::LGFX_Device
+{
+    lgfx::Panel_ILI9341 _panel;
+    lgfx::Bus_SPI _bus;
+    // lgfx::Light_PWM _light;
+    // lgfx::Touch_XPT2046 _touch;
+
+public:
+    LGFX()
+    {
+        // SPI Bus
+        {
+            auto cfg = _bus.config();
+
+            cfg.spi_host = 0;      // RP2040 SPI0
+            cfg.spi_mode = 0;
+            cfg.freq_write = 40000000;
+            cfg.freq_read = 16000000;
+
+            // cfg.pin_sclk = 18;
+            // cfg.pin_mosi = 19;
+            // cfg.pin_miso = 16;
+            cfg.pin_dc   = 15;
+            cfg.pin_sclk = 2;
+            cfg.pin_mosi = 3;
+            cfg.pin_miso = 4;
+
+            _bus.config(cfg);
+            _panel.setBus(&_bus);
+        }
+
+        // LCD
+        {
+            auto cfg = _panel.config();
+
+            cfg.pin_cs  = 5;
+            cfg.pin_rst = 14;
+
+            cfg.panel_width  = SCREEN_WIDTH;
+            cfg.panel_height = SCREEN_HEIGHT;
+
+            cfg.memory_width  = SCREEN_WIDTH;
+            cfg.memory_height = SCREEN_HEIGHT;
+
+            cfg.offset_x = 0;
+            cfg.offset_y = 0;
+            cfg.offset_rotation = 0;
+
+            // cfg.readable = true;
+            cfg.invert = false;
+            cfg.rgb_order = false;
+
+            _panel.config(cfg);
+        }
+
+        // // Backlight
+        // {
+        //     auto cfg = _light.config();
+
+        //     cfg.pin_bl = 22;
+        //     cfg.invert = false;
+
+        //     _light.config(cfg);
+        //     _panel.setLight(&_light);
+        // }
+
+        // Touch
+        // {
+        //     auto cfg = _touch.config();
+
+        //     cfg.x_min = 0;
+        //     cfg.x_max = 240;
+
+        //     cfg.y_min = 0;
+        //     cfg.y_max = 320;
+
+        //     cfg.spi_host = 0;
+        //     cfg.freq = 2500000;
+
+        //     cfg.pin_sclk = 2;
+        //     cfg.pin_mosi = 3;
+        //     cfg.pin_miso = 4;
+
+        //     cfg.pin_cs = 15;
+
+        //     cfg.bus_shared = true;
+
+        //     // _touch.config(cfg);
+        //     // _panel.setTouch(&_touch);
+        // }
+
+        setPanel(&_panel);
+    }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,71 @@
 #include <Arduino.h>
+#include <lvgl.h>
+#include <TFT_eSPI.h>
 
-void setup() {
+TFT_eSPI tft = TFT_eSPI();
 
+static const uint16_t W = 240;
+static const uint16_t H = 320;
+
+enum { SCREENBUFFER_SIZE_PIXELS = W * H / 20 };
+
+static lv_color_t buf[SCREENBUFFER_SIZE_PIXELS];
+
+void flush_cb(lv_display_t *disp, const lv_area_t *area, uint8_t *px_map)
+{
+    uint32_t w = (area->x2 - area->x1 + 1);
+    uint32_t h = (area->y2 - area->y1 + 1);
+
+    tft.startWrite();
+    tft.setAddrWindow(area->x1, area->y1, w, h);
+    tft.pushColors((uint16_t *)px_map, w * h, true);
+    tft.endWrite();
+
+    lv_display_flush_ready(disp);
 }
 
-void loop() {
+/*Set tick routine needed for LVGL internal timings*/
+static uint32_t my_tick_get_cb (void) { return millis(); }
 
+void setup()
+{
+    Serial.begin(115200);
+
+    lv_init();
+
+    Serial.println("BOOT");
+
+    tft.begin();
+    tft.setRotation(0);
+    // tft.setSwapBytes(true);
+
+
+    static lv_disp_t *disp;
+    disp = lv_display_create(W, H);
+
+    lv_display_set_color_format(disp, LV_COLOR_FORMAT_RGB565);
+
+    lv_display_set_buffers(
+        disp,
+        buf,
+        NULL,
+        sizeof(buf),
+        LV_DISPLAY_RENDER_MODE_PARTIAL
+    );
+
+    lv_display_set_flush_cb(disp, flush_cb);
+
+    lv_tick_set_cb(my_tick_get_cb);
+
+    lv_obj_t *label = lv_label_create(lv_screen_active());
+    lv_label_set_text(label, "LVGL 9 OK");
+    lv_obj_center(label);
+
+    Serial.println("READY");
+}
+
+void loop()
+{
+    lv_timer_handler();
+    delay(5);
 }
